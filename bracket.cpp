c6573539ac9b7e92a592e65e586f97abc391792c
ann_dovidova <annadovidova04@mail.ru>
2024-05-11 19:55:24 +0300

    task5.2

---
+++ bracket.cpp
@@ -0,0 +1,21 @@
+#include "bracket.h"
+using namespace std;
+
+bool checkBrackets(const std::string& s) {
+	const char* delim[2] = { "([{<", ")]}>" };
+	string stack = "";
+	for (int i = 0; i < s.size(); i++) {
+		const char c = s[i];
+		if (strchr(delim[0], c) != NULL) {
+			stack.push_back(c);
+			continue;
+		}
+		const char* z;
+		if (NULL == (z = strchr(delim[1], c)))
+			continue;
+		if (stack.empty() || stack.back() != delim[0][z - delim[1]])
+			return false;
+		stack.pop_back();
+	}
+	return stack.empty();
+}
